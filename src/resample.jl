0a182f6c42ca0b9363d526505123608afb00c2c7
femtotrader <femto.trader@gmail.com>
2016-11-10 21:51:14 +0100

    Fix timestamp (closes #7)

--- src/resample.jl
+++ src/resample.jl
@@ -23,13 +23,16 @@ function ohlc(resampler::TimeArrayResampler)
     ta_l = collapse(ta, f_group, first, minimum)
     ta_c = collapse(ta, f_group, first, last)
     a_ohlc = hcat(ta_o.values, ta_h.values, ta_l.values, ta_c.values)
-    ta_ohlc = TimeArray(ta_o.timestamp, a_ohlc, ["Open", "High", "Low", "Close"])
+    ts = map(f_group, ta_o.timestamp)
+    ta_ohlc = TimeArray(ts, a_ohlc, ["Open", "High", "Low", "Close"])
 end
 
 function mean(resampler::TimeArrayResampler)
-    collapse(resampler.ta, dt_grouper(resampler.tf), first, mean)
+    f_group = dt_grouper(resampler.tf)
+    collapse(resampler.ta, f_group, dt -> f_group(first(dt)), mean)
 end
 
 function sum(resampler::TimeArrayResampler)
-    collapse(resampler.ta, dt_grouper(resampler.tf), first, sum)
+    f_group = dt_grouper(resampler.tf)
+    collapse(resampler.ta, f_group, dt -> f_group(first(dt)), sum)
 end
